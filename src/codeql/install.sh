173d8084d8ed69e9347c351bcf9777e65925593f
Mateus Perdigão <mateusperdigao@hotmail.com>
2024-11-06 13:00:42 +0000

    test

--- src/codeql/install.sh
+++ src/codeql/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 CODEQL_HOME=/usr/local/codeql-home
-
+CODEQL_TAG=""
 echo "Activating feature 'CodeQL'"
 echo "The provided CodeQL version is: $CODEQL_VERSION"
 echo "The CodeQL home directory is: $CODEQL_HOME"
@@ -20,52 +20,16 @@ echo "The effective dev container containerUser is '$_CONTAINER_USER'"
 echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
 
 
-get_valid_releases() {
-    echo "Getting valid CodeQL releases from GitHub API"
-
-    page=1
-    releases=""
-
-    while true; do
-        # Fetch a page of releases
-        response=$(curl -s "https://api.github.com/repos/github/codeql-action/releases?per_page=100&page=$page")
-        echo "debug: response: $response"
-
-        # Check if there are no more releases
-        if [ "$(echo "$response" | jq '. | length')" -eq 0 ]; then
-            break
-        fi
-
-        # Extract valid releases and add to the list
-        valid_releases=$(echo "$response" | jq -r '.[].tag_name' | grep '^codeql-bundle-v')
-        releases="${releases}${valid_releases}\n"
-
-        # Move to the next page
-        page=$((page + 1))
-    done
-
-    # Print all valid releases, one per line
-    echo "Valid CodeQL releases:"
-    printf "%s" "$releases"
+check_version() {
+    echo "Checking version"
 
     # Set the codeql version to the latest if it is not provided
     if [ "$CODEQL_VERSION" = "latest" ]; then
-        # Get the latest release
-        CODEQL_LATEST=$(echo "$releases" | head -n 1)
-        CODEQL_VERSION=${CODEQL_LATEST#codeql-bundle-v}  # Remove the prefix 'codeql-bundle-v'
-        echo "Setting the CodeQL version to the latest: $CODEQL_VERSION"
-    fi
-}
-
-check_version() {
-    echo "Checking if the provided version is valid"
-
-    # Check if the current version is in the list of valid releases
-    if get_valid_releases | grep -qx "codeql-bundle-v$CODEQL_VERSION"; then
-        echo "The provided CodeQL version ($CODEQL_VERSION) is valid."
+        CODEQL_TAG=$(curl -s https://api.github.com/repos/github/codeql-action/releases/latest | jq -r '.tag_name')
+        echo "Setting the CodeQL version to the latest: $CODEQL_TAG"
     else
-        echo "The provided CodeQL version ($CODEQL_VERSION) is not valid."
-        exit 1
+        CODEQL_TAG="codeql-bundle-v$CODEQL_VERSION"
+        echo "Using provided version: $CODEQL_TAG"
     fi
 }
 
